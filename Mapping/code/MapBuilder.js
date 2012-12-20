bd0e0271727c08c08d37154916545ada774f979e
Robby <geomasta21@aol.com>
2012-12-20 11:21:28 -0600

    added basic mapping code

---
+++ Mapping/code/MapBuilder.js
@@ -0,0 +1,89 @@
+var sin = Math.sin, cos = Math.cos, abs = Math.abs;
+
+var map_rows = 100,
+    map_cols = 100;
+
+function MapBuilder(x, y, heading, width) {
+    this.x = x;
+    this.y = y;
+    this.heading = heading;
+    this.width = width;
+    
+    this.col_inc = CANVAS_WIDTH/map_cols;
+    this.row_inc = CANVAS_HEIGHT/map_rows;
+    
+    this.grid = new Array(map_rows);
+    for (var r = 0; r < map_rows; r++) {
+        this.grid[r] = new Array(map_cols);
+        for (var c = 0; c < map_cols; c++) {
+            this.grid[r][c] = 0;
+        }
+    }
+    
+    this.apply_lidar_data = function(lidar) {
+		for (var i = 0; i < lidar.num_angles; i++) {
+            var mag = lidar.val[i];
+        
+            if (mag < lidar.MAX_VAL-1) {
+                var angle = this.heading + lidar.start_angle + lidar.inc*i,
+                    point_x = this.x + mag*cos(angle),
+                    point_y = this.y + mag*sin(angle),
+                    col = Math.floor(point_x/this.col_inc),
+                    row = Math.floor(point_y/this.row_inc);
+                
+				if (this.grid[row][col] < 1) {
+                	this.grid[row][col] += .01;
+				}
+            }
+        }
+    };
+    
+    this.update = function(Vl, Vr, lidar, dt) {
+        var x = this.x, y = this.y, theta = this.heading, L = this.width;
+        var new_x, new_y, new_heading;
+            
+        if (abs(Vl - Vr) < .00001) {
+            new_x = x + Vl*dt*cos(theta);
+            new_y = y + Vl*dt*sin(theta);
+            new_heading = theta;
+        } else {
+            // Credit: Dudek and Jenkin, Computational Principles of Mobile Robotics
+            var R = L*(Vl + Vr)/(2*(Vr - Vl)),
+                wd = dt*(Vr - Vl)/L;
+            new_x = x + R*cos(wd)*sin(theta) + R*sin(wd)*cos(theta) - R*sin(theta);
+            new_y = y + R*sin(wd)*sin(theta) - R*cos(wd)*cos(theta) + R*cos(theta);
+            new_heading = theta + wd;
+        }
+            
+        new_heading = new_heading%(2*PI);
+        
+        this.x = new_x;
+        this.y = new_y;
+        this.heading = new_heading;
+        
+        this.apply_lidar_data(lidar);
+    };
+    
+    this.draw = function(context) {
+        context.fillStyle = "#FFFFFF";
+        for (var r = 0; r < map_rows; r++) {
+            for (var c = 0; c < map_cols; c++) {
+                if (this.grid[r][c] > 0) {
+					context.fillRect(c*this.col_inc, r*this.row_inc, this.col_inc, this.row_inc);
+                }
+            }
+        }
+        
+        context.fillStyle = "blue";
+        context.beginPath();
+        context.arc(this.x, this.y, 4, 0, Math.PI*2, false);
+        context.fill();
+            
+        context.strokeStyle = "blue";
+        context.beginPath();
+        context.moveTo(this.x, this.y);
+        context.lineTo(this.x + 20*Math.cos(this.heading), 
+                       this.y + 20*Math.sin(this.heading));
+        context.stroke();
+    };
+}
---
+++ Mapping/code/main.js
@@ -0,0 +1,198 @@
+var PI = Math.PI, MAX_V = .1, DELTA_ALPHA = PI/20, DELTA_V = .005, CANVAS_WIDTH, CANVAS_HEIGHT, 
+    KEY_w = 87, KEY_s = 83, KEY_a = 65, KEY_d = 68, KEY_space = 32, KEY_e = 69, KEY_q = 81;
+
+var canvas, context, perception_context;
+var robot, timekeeper, obstacles, map_builder;
+
+var num_obstacles = 10,
+    obstacle_pad = 20,
+    obstacle_radius = 20;
+	raytracecols = 20,        
+    raytracerows = 20,
+    map_builder_period = 10;
+
+window.onload = function() 
+{
+    canvas = document.getElementById("reality");
+    CANVAS_WIDTH = canvas.width;
+    CANVAS_HEIGHT = canvas.height;
+	context = canvas.getContext("2d");   
+	perception_context = document.getElementById("perception").getContext("2d");  
+    
+    make_obstacles();
+   	make_robot(); 
+   	map_builder = new MapBuilder(robot.x, robot.y, robot.heading, robot.width);
+
+    setInterval(paintCanvas, 30);
+                       
+    setTimeout(update_builder, map_builder_period);
+}
+
+var prev_time = 0;
+
+function update_builder() {
+    var dt = 0,
+        cur_time = new Date().getTime();
+    if (prev_time != 0) {
+        dt = cur_time - prev_time;
+    }
+    prev_time = cur_time;
+    
+    map_builder.update(robot.wheel1_velocity, 
+                       robot.wheel2_velocity, 
+                       robot.lidar_sensor, 
+                       dt);
+                       
+    setTimeout(update_builder, map_builder_period);
+}
+
+function keydown(event) 
+{
+    var key = event.which;
+    
+    var delta_wheel1_velocity = 0, 
+        delta_wheel2_velocity = 0;
+    
+    if (key == KEY_w) {
+        delta_wheel1_velocity = DELTA_V;
+        delta_wheel2_velocity = DELTA_V;
+    } else if (key == KEY_s) {
+        delta_wheel1_velocity = -DELTA_V;
+        delta_wheel2_velocity = -DELTA_V;
+    } else if (key == KEY_a) {
+        delta_wheel1_velocity = DELTA_V;
+        delta_wheel2_velocity = -DELTA_V;
+    } else if (key == KEY_d) {
+        delta_wheel1_velocity = -DELTA_V;
+        delta_wheel2_velocity = DELTA_V;
+    } else if (key == KEY_space) {
+        delta_wheel1_velocity = -robot.wheel1_velocity;
+        delta_wheel2_velocity = -robot.wheel2_velocity;
+    } else if (key == KEY_e) {
+        my_particle_filter.verbosity_level = (my_particle_filter.verbosity_level+1)%3;
+    } else if (key == KEY_q) {
+        my_kalman_filter.verbosity_level = (my_kalman_filter.verbosity_level+1)%2;
+    } else {
+        return;
+    }
+    
+    robot.update();
+    robot.accelerate_wheels(delta_wheel1_velocity, delta_wheel2_velocity); 
+}
+
+function paintCanvas() 
+{
+    context.fillStyle = "lightGray";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    
+    map_builder.draw(context);
+    
+    perception_context.fillRect(0, 0, canvas.width, canvas.height);
+    map_builder.draw(perception_context);
+ 
+    context.fillStyle = "black";
+    context.strokeStyle = "black";
+    for (var i = 0; i < robot.sensors.length; i++) {
+        robot.sensors[i].draw(context);
+    }
+    
+    context.strokeStyle = "black";
+    context.lineWidth = 3;
+    for (var i = 0; i < obstacles.length; i++) {
+        obstacles[i].draw(context);
+    }
+    
+    robot.update();
+    context.strokeStyle = "green";
+    robot.draw(context);
+}
+
+function make_robot() {
+    timekeeper = realtime_timekeeper();
+
+    robot = tank_robot(
+                CANVAS_WIDTH/2,
+                CANVAS_HEIGHT/2,
+                0,
+                0, 
+                0, 
+                40*SCALE, 
+                40*SCALE, 
+                0, 
+                timekeeper
+                );
+    
+    var raytracer = create_raytracer(
+                        raytracecols, 
+						raytracerows, 
+                        CANVAS_WIDTH/raytracecols, 
+                        CANVAS_HEIGHT/raytracerows, 
+                        obstacles);
+    
+    robot.lidar_sensor = lidar_sensor(
+                            raytracer, 
+                            robot, 
+                            0, 
+                            0, 
+                            0, 
+                            500, 
+                            0, 
+                            -3*PI/4, 
+                            3*PI/4, 
+                            270
+                            );
+        
+    robot.sensors = [robot.lidar_sensor];
+}
+
+function make_obstacles() {
+	obstacles = new Array(num_obstacles);
+
+	for (var i = 0; i < num_obstacles; i++) {
+        var seedx = Math.random()*(CANVAS_WIDTH - 2*obstacle_pad) + obstacle_pad,
+            seedy = Math.random()*(CANVAS_HEIGHT - 2*obstacle_pad) + obstacle_pad,
+            a1 = Math.random()*Math.PI*2/3,
+            a2 = Math.random()*Math.PI*2/3,
+            a3 = Math.random()*Math.PI*2/3;
+            
+        obstacles[i] = obstacle(create_polygon([
+          create_point(seedx + obstacle_radius*Math.cos(a1), 
+                       seedy + obstacle_radius*Math.sin(a1)),
+          create_point(seedx + obstacle_radius*Math.cos(a1+a2), 
+                       seedy + obstacle_radius*Math.sin(a1+a2)),
+          create_point(seedx + obstacle_radius*Math.cos(a1+a2+a3), 
+                       seedy + obstacle_radius*Math.sin(a1+a2+a3)),
+        ]));
+    };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
